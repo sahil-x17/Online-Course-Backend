5580ebdf7b740e3aa308c5456d0a104baf7e9277
Sahil Gedam <139269014+sahil-x17@users.noreply.github.com>
2024-10-03 23:18:05 +0530

    Add files via upload

---
+++ db/index.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+// Connect to MongoDB
+mongoose.connect(
+  "mongodb+srv://gedamsahil001:Xgw0LL3zJ8fEuMp4@cluster0.zk0cd.mongodb.net/course_selling_app2"
+);
+
+// Define schemas
+const AdminSchema = new mongoose.Schema({
+  // Schema definition here
+  username: String,
+  password: String,
+});
+
+const UserSchema = new mongoose.Schema({
+  // Schema definition here
+  username: String,
+  password: String,
+  purchasedCourses: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+    },
+  ],
+});
+
+const CourseSchema = new mongoose.Schema({
+  // Schema definition here
+  title: String,
+  description: String,
+  price: Number,
+  imageLink: String,
+});
+
+const Admin = mongoose.model("Admin", AdminSchema);
+const User = mongoose.model("User", UserSchema);
+const Course = mongoose.model("Course", CourseSchema);
+
+module.exports = {
+  Admin,
+  User,
+  Course,
+};
